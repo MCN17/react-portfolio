804d0778e32735f5ac24063847c3b146e3f920e7
Marty <martynolan17@gmail.com>
2022-10-07 14:54:08 -0400

    added Contact component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import About from "./components/About";
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
+  
   return (
     <div>
       <Navbar></Navbar>
       <main>
         <About></About>
         <Projects></Projects>
+        <Contact></Contact>
       </main>
     </div>
   );
