89c9b5378b9576330bbbac2f3a741ecf6a7c74df
Marty <martynolan17@gmail.com>
2022-10-07 14:24:10 -0400

    added Navbar/Projects components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './App.css';
 import About from "./components/About";
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <main>
         <About></About>
+        <Projects></Projects>
       </main>
     </div>
   );
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import portfolioPic from "../../assets/portfolio-pic.jpg";
+import "./about.css";
 
 function About() {
     return (
-        <section>
+        <div className="about">
             <h1>About</h1>
             <img src={portfolioPic} alt="close up of face"></img>
             <p> Web developer with a passion for working in teams and a background in sales, customer service, and home renovations. Currently pursuing a certificate in full stack web development from Carleton University and developed skills in HTML5, JavaScript, and CSS.
                 Excelled in the sales industry, being able to problem solve and reach goals in high stress environments. Recipient of the Leader of the Year Award in 2012. Collaborated with team members to complete home renovation projects and developed an attention to detail.
                 Enjoy the process of creating web applications and the research that goes into learning new methods to make them more efficient. Excited to contribute to projects and bring the teamwork skills gained to a company.</p>
-        </section>
+        </div>
     );
 }
 
