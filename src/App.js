54e11c66a3aa415f3ee1b5a5ca7e3b6d54d9f1ae
Marty <martynolan17@gmail.com>
2022-10-10 10:48:33 -0400

    added Resume/Footer components + routes to each page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import About from "./components/About";
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
 function App() {
   
   return (
-    <div>
-      <Navbar></Navbar>
-      <main>
-        <About></About>
-        <Projects></Projects>
-        <Contact></Contact>
-      </main>
-    </div>
+    <Router>
+      <div>
+        <Navbar></Navbar>
+        <main>
+          <Routes>
+            <Route path="/" element={<About></About>}></Route>
+            <Route path="/projects" element={<Projects></Projects>}></Route>
+            <Route path="/contact" element={<Contact></Contact>}></Route>
+            <Route path="/resume" element={<Resume></Resume>}></Route>
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import "./navbar.css";
 
 function Navbar() {
     return (
         <nav className="navbar">
             <ul>
-                <li><a href="#about" >About</a></li>
-                <li><a href="#projects" >Projects</a></li>
-                <li><a href="#contact" >Contact</a></li>
-                <li><a href="#resume" >Resume</a></li>
+                <li><Link to="/">About</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/resume">Resume</Link></li>
             </ul>
         </nav>
     )
