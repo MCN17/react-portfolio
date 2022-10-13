414eee9942b83f241ab3ba952ea3b5affadd9f81
Marty <martynolan17@gmail.com>
2022-10-13 10:54:15 -0400

    added contact form

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,8 +6,8 @@ function About() {
     return (
         <div className="container">
             <div className="row">
-                <div className="col text-center">
-                    <h1>About</h1>
+                <div className=" about-body col text-center mt-5">
+                    <h1 className="mb-3">About</h1>
                     {/* <img src={portfolioPic} alt="close up of face"></img> */}
                     <p> Web developer with a passion for working in teams and a background in sales, customer service, and home renovations. Currently pursuing a certificate in full stack web development from Carleton University and developed skills in HTML5, JavaScript, and CSS.
                         Excelled in the sales industry, being able to problem solve and reach goals in high stress environments. Recipient of the Leader of the Year Award in 2012. Collaborated with team members to complete home renovation projects and developed an attention to detail.
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import {validateEmail } from "../../utils/helpers";
+import "./contact.css";
 
 function Contact() {
+
+
+    const [formState, setFormState] = useState({ name: "", email: "", message: "" });
+
+    const { name, email, message } = formState;
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    function handleChange(e) {
+
+        if (e.target.name === "email") {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+
+            //isValid conditional statement
+            if (!isValid) {
+                setErrorMessage("Your email is invalid.");
+            } else {
+                setErrorMessage("");
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`)
+            } else {
+                setErrorMessage("");
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value })
+        console.log("errorMessage", errorMessage);
+        }
+    }
+
+    // console.log(formState);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    };
+
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div class="col-4 mt-5">
+                    <div className="text-center">
+                    <h1 className="contact-body mb-3" data-testid="h1tag">Contact me</h1>
+                    </div>
+                    <form id="contact-form" onSubmit={handleSubmit}>
+                        <div className="form-floating mb-2">
+                            <input className="form-control" type="text" defaultValue={name} onBlur={handleChange} name="name" placeholder="Name"></input>
+                            <label htmlFor="name">Name:</label>
+                        </div>
+                        <div className="form-floating mb-2">
+                            <input className="form-control" type="email" defaultValue={email} name="email" onBlur={handleChange} placeholder="email"></input>
+                            <label htmlFor="email">Email address:</label>
+                        </div>
+                        <div className="form-floating mb-2">
+                            <textarea className="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="5" placeholder="message"></textarea>
+                            <label htmlFor="message">Message:</label>
+                        </div>
+                        {errorMessage && (
+                            <div>
+                                <p className="error-text">{errorMessage}</p>
+                            </div>
+                        )}
+                        <button className="btn " data-testid="button" type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
-    );
+    )
+
 }
 
 export default Contact;
